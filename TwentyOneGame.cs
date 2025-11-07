0dde7425ee4e9d1b06e5c243b70aa57706655235
austzdee <167780868+austzdee@users.noreply.github.com>
2025-11-07 09:49:58 +0000

    created an Interface

--- TwentyOneGame.cs
+++ TwentyOneGame.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-     public class TwentyOneGame : Games
+     public class TwentyOneGame : Games, IWalkAway
     {
         public override void Play()
         {
             // Implementation of the game logic goes here
+        }
+        public override void ListPlayers()
+        {
+            // Custom implementation for listing players in TwentyOne game
+            foreach (string player in Players)
+            {
+                Console.WriteLine($"Player: {player}");
+            }
+        }
+
+        public void WalkAway(Player player)
+        {
+            Console.WriteLine($"{player.Name} has chosen to walk away from the game.");
+            // Additional logic for handling the player walking away can be added here
         }
     }
 }
